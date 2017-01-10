af1afeaf26219b39c9117cf8682c29b3ab282608
Jakub Wróblewski <j.wroblewski97@gmail.com>
2017-01-10 21:09:07 +0100

    probably final commit

--- topol.ml
+++ topol.ml
@@ -1,5 +1,5 @@
 (* Autor: Jakub Wróblewski gr. 5 386407    *)
-(* Recenzent: Mateusz Bajorek gr. ? ?????? *)
+(* Recenzent: Mateusz Bajorek gr. 3 385122 *)
 
 (* wyjątek podnoszony gdy graf z wejścia jest cykliczny *)
 exception Cykliczne
@@ -19,7 +19,9 @@ let topol input_list =
     let w = ref [] in
     (* funkcja przechodząca po grafie (DFS) *)
     let rec dfs x =
-        let fnd = PMap.find x !graph in
+        let fnd =
+            if PMap.mem x !graph then PMap.find x !graph
+            else ([], false, false) in
         match fnd with
         | (_, true, false) -> raise Cykliczne
         | (_, true, true) -> ()
@@ -42,4 +44,8 @@ topol [(1, [2;3]); (2, [3]); (3, [])];;
 topol [(1, [2;3]); (2, [3]); (3, [1])];;
 topol [(1, [2]); (2, [3;4]); (3, [5;6;7]); (4, []); (5, []); (6, [8]); (7, []); (8, [9]); (9, [10]); (10, [])];;
 topol [(1, [2]); (2, [3;4]); (3, [5;6;7]); (4, [3]); (5, []); (6, [8]); (7, []); (8, [9]); (9, [10]); (10, [7])];;
+topol [(1, [2]); (2, [3;4]); (3, []); (4, [3]); (5, []); (6, [])];;
+topol [(1, [2;3]); (2, [3;4;5;6;7]); (3, [])];;
+topol [(1, [2;3]); (2, [3;4;5;6;7]); (3, [7])];;
+topol [("t", ["te"; "tes"]); ("te", ["test"; "tes"])];;
 *)
